08435ba08467be85431db2caba0921640a154393
Danielle Kenwood <dkenwood@users.noreply.github.com>
2015-04-06 16:37:24 -0700

    Created header.php

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Dash of Color</title>
+
+      <link href="style.css" rel="stylesheet">
+      <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+      <link href='http://fonts.googleapis.com/css?family=Dosis:300,400' rel='stylesheet' type='text/css'>
+    <script src="modernizr.custom.js"></script>
+    
+      <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+      <script src="smoothscroll.js"></script> 
+      <script src="classie.js"></script>
+      
+  </head>
+
+  <body>
+      
+        <header>
+          
+            <a href="http://dashofcolor.net/" class="logo">Dashof<span class="color">Color</span></a>
+              
+                <nav>
+                    <a href="#about" class="smoothScroll">About</a>
+                    <a href="#projects" class="smoothScroll">Projects</a>
+                </nav>
+            
+        </header>
