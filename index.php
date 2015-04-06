171c3664cdfa57892b204bd9dadf4a3acb00130d
Danielle Kenwood <dkenwood@users.noreply.github.com>
2015-04-06 16:34:27 -0700

    Created home page

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Dash of Color</title>
+
+      <link href="style.css" rel="stylesheet">
+      <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+      <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+    <script src="js/modernizr.custom.js"></script>
+    
+      <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+      <script src="js/smoothscroll.js"></script> 
+      <script src="js/classie.js"></script>
+      
+  </head>
+
+  <body>
+      
+        <header>
+          
+            <a href="http://dashofcolor.net/" class="logo">Dashof<span class="color">Color</span></a>
+              
+                <nav>
+                    <a href="#about" class="smoothScroll">About</a>
+                    <a href="#projects" class="smoothScroll">Projects</a>
+                </nav>
+              </header>
+      
+    <div class="site-wrapper"> 
+        
+      <div class="site-wrapper-inner">
+        
+
+          <span class="title bigger-title">Danielle Kenwood</span><p class="title subtitle">Designer &amp; Developer</p>
+    
+        </div>
+      </div>
+        
+    <div id="about" class="site-wrapper-about"> 
+
+          <div class="boxpadding">
+              <span class="box-title about-title">About</span>
+              <div class="about-text">
+                Hello! I'm Danielle, a designer and developer at University of California, Berkeley, where I study Cognitive Science and Computer Science. I specialize in UI/UX design and web development. When I'm not happily taking on new design or coding challenges, I can be found swimming backstroke on cloudy days, writing short stories, playing with dogs, or going on adventures (via books or airplanes).
+              
+          </div>
+              
+                  <p class="about-nav">
+                      <a href="mailto:danielle@dashofcolor.net" class="button">Email</a>
+                      <a href="http://dashofcolor.net/files/DanielleKenwood%20-%20Resume.pdf" class="button">Resume</a>
+                      <a href="http://www.linkedin.com/in/dkenwood" target="_blank" class="button">LinkedIn</a>
+                  </p>
+        
+        </div>
+      </div>
+      
+    <div id="projects" class="site-wrapper-projects"> 
+
+          <div class="boxpadding">
+              <span class="box-title projects-title">Projects</span>
+              
+				<div class="grid">
+					<figure class="proj-effect">
+						<img src="http://dashofcolor.net/images/mastery.png" alt="Mastery"/>
+                            <figcaption>
+                                <div><a href="http://dashofcolor.net/mastery.php"><h1>Mastery</h1></a>
+                    <p class="project-desc">Personal Website</p>
+							</div>
+						</figcaption>			
+					</figure>
+
+					<figure class="proj-effect">
+						<img src="http://dashofcolor.net/images/dcapply2.png" alt="The Daily Californian: Apply Website"/>
+                            <figcaption>
+							<div>
+                    <a href="http://dashofcolor.net/dcapply.php"><h1>The Daily Cal: Apply</h1></a>
+                  <p class="project-desc">Website Prototype</p>
+							</div>
+							
+						</figcaption>	
+					</figure>
+                    
+					<figure class="proj-effect">
+						<img src="http://dashofcolor.net/images/meetup2.png" alt="Meetup"/>
+                            <figcaption>
+							<div>
+                    <a href="http://dashofcolor.net/meetup.php"><h1>Meetup</h1></a>
+                  <p class="project-desc">Mobile App Prototype</p>
+							</div>
+							
+						</figcaption>	
+					</figure>
+                    
+					<figure class="proj-effect">
+						<img src="http://dashofcolor.net/images/graphicdesign.png" alt="Graphic Design"/>
+                            <figcaption>
+							<div>
+                    <a href="http://dashofcolor.net/graphicdesign.php"><h1>Graphic Design</h1></a>
+                  <p class="project-desc">Miscellaneous</p>
+							</div>
+							
+						</figcaption>	
+					</figure>
+                    
+				</div>
+              
+        </div></div>
+      
+    <div class="site-wrapper-footer"> 
+
+          <div class="footer">
+                <p>&#169; 2015 Danielle Kenwood</p>
+        
+        </div>
+      </div>
+        
+      
+  </body>
+</html>
